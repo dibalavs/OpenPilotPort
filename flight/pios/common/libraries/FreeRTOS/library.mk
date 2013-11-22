fa7b8c0f38fa7f244383fc884d9a22e99ada5ed2
Vasily Dybala <dibalavs@yandex.ru>
2013-11-23 00:02:07 +0400

    Allow set breakpoint inside FreeRTOS code
    	fix path separator on windows platform (FreeRTOS//Sources)
    	modified:   make/common-defs.mk

--- flight/pios/common/libraries/FreeRTOS/library.mk
+++ flight/pios/common/libraries/FreeRTOS/library.mk
@@ -5,6 +5,16 @@
 # has been defined and add in the target-specific pieces separately.
 #
 
+UNAME := $(shell uname)
+# Here and everywhere if not Linux or Mac then assume Windows
+ifeq ($(filter Linux Darwin, $(UNAME)), )
+    UNAME := Windows
+endif 
+
+ifeq ($(UNAME), Windows)
+FREERTOS_DIR	:= $(dir $(lastword $(MAKEFILE_LIST)))Source
+else
 FREERTOS_DIR	:= $(dir $(lastword $(MAKEFILE_LIST)))/Source
-SRC		+= $(sort $(wildcard $(FREERTOS_DIR)/*.c))
+endif
+SRC		+= $(wildcard $(FREERTOS_DIR)/*.c)
 EXTRAINCDIRS	+= $(FREERTOS_DIR)/include
